ba37450342e0d4a29913f91c244475fd97d04205
robert-kuropkat <robert.kuropkat@gmail.com>
2023-11-02 11:27:36 -0400

    End of video 2 Variables

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public string myName     = "Robert";
+    public int    myAge      = 57;
+    public string myLocation = "Florida";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myName = "Fred";
+
+        Debug.Log("My Name is: " + myName);
+        Debug.Log("My age is : " + myAge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
