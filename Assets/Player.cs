92c82ddb5b45d81add109ea88f7eead22abfcdac
Robert Kuropkat <robert@kuropkat.com>
2023-11-02 17:16:39 -0400

    Completed Variable Challenge 1.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,35 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Unity C# Survival Guide - Variable Challenge 1.
+/// </summary>
+/// 
+/// <remarks>
+/// Instantiated By: 
+/// Attached To: Main Camera
+/// 
+/// Create a set of variables described in the Challenge video and print thier values.
+/// I decided to create all the variables with a default value, change the value, then
+/// print them out using Debug.Log().  I used the concatenation operator to merge the
+/// values with text to display all the values as part of a sentance.  For the last 
+/// part (hasKey), I used the ternary operator just because it's one of my favorite
+/// operators.
+/// 
+/// </remarks>
+/// 
+
 public class Player : MonoBehaviour
 {
-
+    //
+    // Setup variables with default values
+    //
     public string myName     = "Robert";
     public int    myAge      = 57;
-    public string myLocation = "Florida";
+    public float  mySpeed    = 5.5f;
+    public int    myHealth   = 10;
+    public int    myScore    = 0;
     public bool   hasKey     = false;
+    public int    myAmmo     = 3;
 
-    public int    score;
-    public float  speed      = 5.5f;
 
-    public GameObject player;
-    public Animator   anim;
-    public Transform  myTransform;
-
-    // Start is called before the first frame update
     void Start()
     {
-        myName = "Fred";
+        //
+        // Change the values of all my variables
+        //
+        myName    = "el Guapo";
+        myAge     = 36;
+        mySpeed   = 2.0f;
+        myHealth  = 3;
+        myScore   = 3;
+        hasKey    = true;
+        myAmmo    = 1;
 
-        Debug.Log("My Name is: " + myName);
-        Debug.Log("My age is : " + myAge);
+        //
+        // Print out current values of all my variables
+        //
+        Debug.Log( "My Name is " + myName
+                 + " and my age is " + myAge + ".  "
+                 + "My current stats are speed " + mySpeed + ", "
+                 + "health " + myHealth + ", "
+                 + "ammo count " + myAmmo + " and "
+                 + "my score is " + myScore + ".  "
+                 );
+        Debug.Log(hasKey ? "I have the key." : "I do not have the key.");
 
-        Debug.Log("My x position: " + myTransform.position.x);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
