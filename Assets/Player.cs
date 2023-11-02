1a7e7d144042c9b36ad319405f6f572c092a50aa
Robert Kuropkat <robert@kuropkat.com>
2023-11-02 15:26:39 -0400

    Completed video 3 Common Datatypes.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,14 @@ public class Player : MonoBehaviour
     public string myName     = "Robert";
     public int    myAge      = 57;
     public string myLocation = "Florida";
+    public bool   hasKey     = false;
+
+    public int    score;
+    public float  speed      = 5.5f;
+
+    public GameObject player;
+    public Animator   anim;
+    public Transform  myTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,8 @@ public class Player : MonoBehaviour
 
         Debug.Log("My Name is: " + myName);
         Debug.Log("My age is : " + myAge);
+
+        Debug.Log("My x position: " + myTransform.position.x);
     }
 
     // Update is called once per frame
