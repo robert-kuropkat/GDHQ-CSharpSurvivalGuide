cad0394ecf3edae803d29a0d9aa323047c001b59
Robert Kuropkat <robert@kuropkat.com>
2023-11-08 15:03:01 -0500

    Completed if-then-else videos and challenges.

---
+++ Assets/Speed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+
+    public int speed = 0;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.S)) { speed++; }
+        if (Input.GetKeyDown(KeyCode.A) && speed != 0) { speed--; }
+
+        if      (speed > 20) { Debug.Log("Slow Down! "       + speed + " is too fast."); } 
+        else if (speed == 0) { Debug.Log("Speed Up! "        + speed + " is too slow."); } 
+        else                 { Debug.Log("Current speed is " + speed); }
+    }
+}
