fce24f2d4f1cf79dfc788537b2bf5d4f174b8a37
Robert Kuropkat <robert@kuropkat.com>
2024-01-07 23:20:41 -0500

    still trying...

--- Assets/CubeArrayRed.cs
+++ Assets/CubeArrayRed.cs
@@ -2,29 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeArrayRed : MonoBehaviour
-{
-    [SerializeField]
-    private GameObject[] playerCubes;
-
-    void Start()
-    {
-        playerCubes = GetCubes();
-    }
-
-    private GameObject[] GetCubes()
-    {
-        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Player");
-
-        foreach ( GameObject cube in cubes)
-        {
-            cube.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
-        }
-
-        return cubes;
-    }
-
-=======
 public class CubeArray : MonoBehaviour
 {
     public GameObject[] cubes;
@@ -43,5 +20,4 @@ public class CubeArray : MonoBehaviour
             }
         }
     }
->>>>>>> dcebb6cd41cf90d1e24547e9d6632094c9d0a41c
 }
